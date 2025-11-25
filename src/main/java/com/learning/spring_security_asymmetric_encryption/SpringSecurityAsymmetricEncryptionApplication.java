3463eb6b786b46b675694625c7dae96a33386072
praneeth reddy <praneethls4008@gmail.com>
2025-11-25 21:51:46 +0530

    init

---
+++ src/main/java/com/learning/spring_security_asymmetric_encryption/SpringSecurityAsymmetricEncryptionApplication.java
@@ -0,0 +1,13 @@
+package com.learning.spring_security_asymmetric_encryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAsymmetricEncryptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAsymmetricEncryptionApplication.class, args);
+	}
+
+}
